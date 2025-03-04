0ecbdda3f455de38080630f667a8857cb1fc01c2
drduh <github@duh.to>
2025-03-03 17:37:28 -0800

    implement port setting

--- cmd/v1/main.go
+++ cmd/v1/main.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"os"
-	"time"
 
 	"github.com/drduh/gone/config"
 	"github.com/drduh/gone/server"
@@ -14,9 +13,9 @@ func Run() {
 	app.Log.Info("started server",
 		"version", app.Version,
 		"host", app.Hostname,
-		"runtime", time.Since(app.Start).String())
+		"port", app.Settings.Port)
 
-	if err := server.Serve(); err != nil {
+	if err := server.Serve(app); err != nil {
 		app.Log.Error("failed to start server",
 			"error", err.Error())
 		os.Exit(1)
--- server/serve.go
+++ server/serve.go
@@ -1,16 +1,18 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/drduh/gone/config"
 	"github.com/drduh/gone/handlers"
 )
 
-func Serve() error {
+func Serve(c *config.App) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.Heartbeat())
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    fmt.Sprintf(":%d", c.Port),
 		Handler: mux,
 	}
 	return srv.ListenAndServe()
