6564d675dd65359de9eb62d16e64c8508451a3c1
drduh <github@duh.to>
2025-03-04 12:43:14 -0800

    demo debug log

--- cmd/v1/main.go
+++ cmd/v1/main.go
@@ -13,6 +13,8 @@ func Run() {
 
 	app.Log.Info("started v1",
 		"version", app.Version, "host", app.Hostname)
+	app.Log.Debug("debug logging enabled",
+		"configuration", app)
 
 	signal.Setup(app)
 
