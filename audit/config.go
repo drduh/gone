cf1ff0474ec1ffa29c3d0a9bc73859e04c496678
drduh <github@duh.to>
2025-03-05 15:18:25 -0800

    customize audit logger with config

--- audit/config.go
+++ audit/config.go
@@ -6,7 +6,20 @@ import (
 	"os"
 )
 
-// Monitor and save application events
+// Auditor configuration
+type Config struct {
+
+	// Whether to output verbose debug messages
+	Debug bool
+
+	// Format for datetime in events
+	TimeFormat string
+
+	// Name of log file to write
+	Filename string
+}
+
+// Monitor for application events
 type Auditor struct {
 
 	// Structured logger for use in app
@@ -15,10 +28,30 @@ type Auditor struct {
 	*log.Logger
 }
 
+// An audit event
+type Event struct {
+
+	// Time of event
+	Time string `json:"time"`
+
+	// Severity level ("INFO", "DEBUG", etc.)
+	Level string `json:"level"`
+
+	// Message (short)
+	Message string `json:"message"`
+
+	// Event data (full)
+	Data map[string]interface{} `json:"data"`
+}
+
+var cfg = &Config{}
+
 // Returns initialized Auditor ready for logging
-func StartAuditor(debug bool) (*Auditor, error) {
+func StartAuditor(c *Config) (*Auditor, error) {
+	cfg = c
+
 	opts := slog.HandlerOptions{}
-	if debug {
+	if cfg.Debug {
 		opts.Level = slog.LevelDebug
 	}
 
