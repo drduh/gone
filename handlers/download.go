92777f6b9f82b27ed1bfddc82a636f94103a95ff
drduh <github@duh.to>
2025-03-06 16:00:58 -0800

    implement basic auth for download

--- handlers/download.go
+++ handlers/download.go
@@ -3,15 +3,25 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/drduh/gone/auth"
 	"github.com/drduh/gone/config"
 )
 
+var errDeny = map[string]string{"error": "not authorized"}
 var errNotFound = map[string]string{"error": "file not found"}
 
 // Returns content by file name
 func Download(app *config.App) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ip, ua := r.RemoteAddr, r.UserAgent()
+
+		if app.Settings.Auth.Require.Download && !auth.Basic(app.Settings.Auth.Basic, r) {
+			writeJSON(w, http.StatusUnauthorized, errDeny)
+			app.Log.Error("not authorized",
+				"ip", ip, "ua", ua)
+			return
+		}
+
 		fileName := r.URL.Query().Get("name")
 
 		var file *config.File
