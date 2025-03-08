6f5cabe2780a34a56a90e33def7b2e5b45a6b779
drduh <github@duh.to>
2025-03-08 15:07:21 -0800

    throttle handler for upload

---
+++ handlers/throttle.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"time"
+
+	"github.com/drduh/gone/config"
+)
+
+// Whether to throttle requests based on rate limit
+func throttle(app *config.App) bool {
+	if app.Settings.Limits.UploadsPM <= 0 {
+		return true
+	}
+
+	now := time.Now()
+	cutoff := now.Add(-1 * time.Minute)
+
+	app.Storage.Throttle.Lease.Lock()
+	defer app.Storage.Throttle.Lease.Unlock()
+
+	validTimes := make([]time.Time, 0, len(app.Storage.Throttle.Times))
+	for _, t := range app.Storage.Throttle.Times {
+		if t.After(cutoff) {
+			validTimes = append(validTimes, t)
+		}
+	}
+
+	if len(validTimes) >= app.Settings.Limits.UploadsPM {
+		return false
+	}
+
+	validTimes = append(validTimes, now)
+	app.Storage.Throttle.Times = validTimes
+
+	return true
+}
