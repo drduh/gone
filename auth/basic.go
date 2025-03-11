bed4557d546bf5f474152afbeb3f75f4a172a936
drduh <github@duh.to>
2025-03-10 19:50:32 -0700

    auth form support, update tests

--- auth/basic.go
+++ auth/basic.go
@@ -1,22 +1,37 @@
 package auth
 
-import "net/http"
-
-const HEADER = "X-Auth"
+import (
+	"fmt"
+	"net/http"
+)
 
 // Returns true if request header contains valid token
-func Basic(token string, r *http.Request) bool {
+func Basic(header, token string, r *http.Request) bool {
+	fmt.Println("header: ", header)
 
 	// Always allow access if token is not configured
 	if token == "" {
 		return true
 	}
 
+	fmt.Println("header: ", header)
+
 	// Check header for non-empty token and validate
-	tokenHeader := r.Header.Get(HEADER)
+	tokenHeader := r.Header.Get(header)
+	fmt.Println("got token: ", tokenHeader)
 	if tokenHeader != "" {
+		fmt.Println("got token: ", tokenHeader)
 		return tokenHeader == token
 	}
+
+	// Check form field value
+	tokenForm := r.FormValue(header)
+	if tokenForm != "" {
+		fmt.Println("got token: ", tokenHeader)
+		return tokenForm == token
+	}
+
+	fmt.Println("no token, returning false")
 
 	return false
 }
