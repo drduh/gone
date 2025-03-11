576ac1be55e51e0745e8b9a9ff1405f5433f9367
drduh <github@duh.to>
2025-03-10 18:24:01 -0700

    index page template

---
+++ templates/index.go
@@ -0,0 +1,25 @@
+package templates
+
+import _ "embed"
+
+//go:embed html/index.html
+var IndexPage string
+
+// Index HTML page elements
+type Index struct {
+
+	// Page title
+	Title string
+
+	// Upload handler path
+	PathUpload string
+
+	// Whether upload requires auth
+	AuthUpload bool
+
+	// Authentication header
+	AuthHeader string
+
+	// Authentication form field placeholder
+	AuthHolder string
+}
