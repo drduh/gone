1ab20d90aa9b72fdb578c674960c1d4405e6c798
drduh <github@duh.to>
2025-03-10 19:52:22 -0700

    remove debug

--- auth/basic.go
+++ auth/basic.go
@@ -1,37 +1,26 @@
 package auth
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
 // Returns true if request header contains valid token
 func Basic(header, token string, r *http.Request) bool {
-	fmt.Println("header: ", header)
 
 	// Always allow access if token is not configured
 	if token == "" {
 		return true
 	}
 
-	fmt.Println("header: ", header)
-
 	// Check header for non-empty token and validate
 	tokenHeader := r.Header.Get(header)
-	fmt.Println("got token: ", tokenHeader)
 	if tokenHeader != "" {
-		fmt.Println("got token: ", tokenHeader)
 		return tokenHeader == token
 	}
 
 	// Check form field value
 	tokenForm := r.FormValue(header)
 	if tokenForm != "" {
-		fmt.Println("got token: ", tokenHeader)
 		return tokenForm == token
 	}
-
-	fmt.Println("no token, returning false")
 
 	return false
 }
