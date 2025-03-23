86d8ec559b6152d76276f0165d03cc9a05c06795
drduh <github@duh.to>
2025-03-23 09:39:26 -0700

    test time functions

--- util/string.go
+++ util/string.go
@@ -6,6 +6,9 @@ import (
 
 // Returns true if string only contains numbers
 func IsNumeric(s string) bool {
+	if s == "" {
+	  return false
+  }
 	for _, r := range s {
 		if !unicode.IsDigit(r) {
 			return false
