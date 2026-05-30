f6cab31661c49cc8ab1b9e0b5a14f9819e0010db
drduh <github@duh.to>
2026-05-30 11:20:00 -0700

    handle non-terminal signals

--- signal/handler.go
+++ signal/handler.go
@@ -10,14 +10,29 @@ import (
 	"github.com/drduh/gone/config"
 )
 
-// Setup configures and listens for terminal signals,
-// logging them before successfully exiting.
+// Setup configures and listens for signals.
 func Setup(app *config.App) {
-	sigChan := make(chan os.Signal, 1)
-	signals := []os.Signal{os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT}
+	sigChan := make(chan os.Signal, 2)
+	signals := []os.Signal{
+		os.Interrupt,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+		syscall.SIGUSR1,
+	}
 	signal.Notify(sigChan, signals...)
+
 	go func() {
-		s := <-sigChan
-		app.Stop(s.String())
+		for {
+			s := <-sigChan
+			switch s {
+			case syscall.SIGUSR1:
+				app.Log.Info("clearing storage",
+					"signal", s)
+				app.ClearStorage()
+			default:
+				app.Stop(s.String())
+				return
+			}
+		}
 	}()
 }
