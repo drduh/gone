7da222bff26b7059a16a0db7ebfc37ca057cb121
drduh <github@duh.to>
2026-05-30 10:50:00 -0700

    patch templates, blank favicon

--- version/version.go
+++ version/version.go
@@ -3,15 +3,15 @@ package version
 // Get returns application version and build details.
 func Get() map[string]string {
 	return map[string]string{
-		"id":      Id,
-		"version": Version,
-		"arch":    Arch,
-		"go":      Go,
-		"commit":  Commit,
-		"path":    Path,
-		"time":    Time,
-		"host":    Host,
-		"system":  System,
-		"user":    User,
+		"appId":         Id,
+		"appVersion":    Version,
+		"buildArch":     Arch,
+		"buildCommit":   Commit,
+		"buildGoVers":   Go,
+		"buildHostname": Host,
+		"buildPath":     Path,
+		"buildSystem":   System,
+		"buildTime":     Time,
+		"buildUser":     User,
 	}
 }
