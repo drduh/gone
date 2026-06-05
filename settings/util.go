2bb1474fac7dd3941952a2c27bbf98d5218d0600
drduh <github@duh.to>
2026-06-04 20:30:00 -0700

    support ipv6 server addr and test

--- settings/util.go
+++ settings/util.go
@@ -1,19 +1,25 @@
 package settings
 
-import "fmt"
+import (
+	"net"
+	"strconv"
+)
 
-// GetAddr returns the server address based on configured settings.
+// GetAddr returns the configured server host
+// and port as an address string.
 func (s *Settings) GetAddr() string {
-	return fmt.Sprintf("%s:%d", s.ServerAddr, s.ServerPort)
+	return net.JoinHostPort(
+		s.ServerAddr, strconv.Itoa(s.ServerPort))
 }
 
-// GetMaxFileBytes returns the maximum allowed file size in bytes.
+// GetMaxFileBytes returns the configured
+// per-file size limit in bytes.
 func (l *Limit) GetMaxFileBytes() int64 {
 	return l.FileLimits.SizeEachMb << 20
 }
 
-// GetMaxTotalFilesBytes returns the maximum allowed size of all
-// files in bytes.
+// GetMaxTotalFilesBytes returns the configured
+// total file size limit in bytes.
 func (l *Limit) GetMaxTotalFilesBytes() int64 {
 	return l.FileLimits.SizeTotalMb << 20
 }
